34983c544909160b9c3a1e7347cfafb67ff913a6
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2026-02-20 08:55:13 +0000

    Stripped back msgraph overalp

--- tests/Database/migrations/0001_00_00_000000_create_ms_graph_tokens_table.php
+++ tests/Database/migrations/0001_00_00_000000_create_ms_graph_tokens_table.php
@@ -26,10 +26,6 @@ return new class extends Migration {
 
     public function down(): void
     {
-        Schema::table('ms_graph_tokens', function (Blueprint $table) {
-            $table->dropForeign('fk_token_user');
-        });
-
         Schema::dropIfExists('ms_graph_tokens');
     }
 };
