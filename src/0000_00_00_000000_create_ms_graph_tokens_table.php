ae0bba725f6319c0f325882675b8e564fbd50916
Anthony Edmonds <anthony.edmonds@live.co.uk>
2026-02-20 07:53:48 +0000

    Create 0000_00_00_000000_create_ms_graph_tokens_table.php

---
+++ src/0000_00_00_000000_create_ms_graph_tokens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('ms_graph_tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('email')->nullable();
+            $table->text('access_token');
+            $table->text('refresh_token')->nullable();
+            $table->string('expires');
+            $table->timestamps();
+
+            $table->foreign('user_id', 'fk_token_user')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ms_graph_tokens');
+    }
+};
